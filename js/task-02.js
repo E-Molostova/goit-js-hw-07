27032470d015b9f6eb22382dbb5a8c8a9761bbb6
E-Molostova <molostova8973@gmail.com>
2021-08-17 19:13:32 +0300

    done 2task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+const makeIngredientsList = ingredients => {
+  return ingredients.map(ingredient => {
+    const ingredientRef = document.createElement('li');
+    ingredientRef.textContent = ingredient;
+    return ingredientRef;
+  });
+};
+
+const itemsOfIngredients = makeIngredientsList(ingredients);
+ingredientsListRef.append(...itemsOfIngredients);
